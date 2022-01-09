d808193e51714438961e7782b81289999cdc6c4f
Bernard <bernard.jablonski@gmail.com>
2022-01-09 22:38:25 +0100

    Układ plików

--- Excel/working.py
+++ Excel/working.py
@@ -1,1 +1,8 @@
+from openpyxl import workbook, load_workbook
 
+wb = load_workbook('Excel/Grandes.xlsx')
+ws = wb.active
+ws['A2'].value = "test"
+print(wb.sheetnames)
+
+wb.save('Excel/Grandes.xlsx')
---
+++ main.py
@@ -0,0 +1,6 @@
+#print("I love pizza")
+#print("It's really good!")
+
+print(type("int"))
+print(type(394))
+print(type(2.12))
